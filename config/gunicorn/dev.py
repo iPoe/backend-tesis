48308b974d1fb5693db2ae883bfd5cf3af3df816
Leonardo <leonardosawi12@gmail.com>
2022-02-18 10:28:28 -0500

    add application name to dev config

--- config/gunicorn/dev.py
+++ config/gunicorn/dev.py
@@ -1,7 +1,7 @@
 """Gunicorn *development* config file"""
 
 # Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
-wsgi_app = "salud_publica.wsgi:application"
+wsgi_app = "salud_publica.wsgi:salud_publica"
 # The granularity of Error log outputs
 loglevel = "debug"
 # The number of worker processes for handling requests
