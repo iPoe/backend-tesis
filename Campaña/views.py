4d7824c9f0c7aae21457559bc8c925a8ec7bc334
iPoe <leonardosawi12@gmail.com>
2022-07-20 19:33:44 -0500

    New logic when user replys to wp msg

--- Campaña/views.py
+++ Campaña/views.py
@@ -381,12 +381,11 @@ def reply_whatsapp(request):
 					for m in medsxcamp:
 						medio = Medio.objects.get(pk = m.medio_id.id)
 						if medio.tipo_medio.descripcion == 5:
-							clientWhatsapp.send_message( medio.sms_mensaje ,
-							request.data['WaId'],'Reply de wp')
-							tipoRes = Tipo_resultado.objects.get( descripcion = "si" )
+							tipoRes = Tipo_resultado.objects.get( descripcion = "r" )
 							res = resultadosxcampania.objects.update_or_create( contacto_cc=usuaria.identidad,
 							 campania_id = c.id, medio_id= medio.id, defaults=	{'Tipo_resultado' : tipoRes} )
-							res.save()
+							clientWhatsapp.send_message( medio.sms_mensaje ,
+							request.data['WaId'])
 							return JsonResponse("Respuesta de whatsapp enviada",status=201,safe=False)
 
 		except Exception as e:
