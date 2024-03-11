42b58155ec006a85ef14e408ccd928fe2e4e4e58
Leo <leonardosawi12@gmail.com>
2024-03-10 20:04:42 -0500

    Go back the old ways

--- Campaña/tasks.py
+++ Campaña/tasks.py
@@ -18,7 +18,7 @@ from .twilioAPI import VoiceCall,SMS,Email,WhatsApp
 clientSMS = SMS()
 clientVoice = VoiceCall()
 clientWhatsapp = WhatsApp()
-whatsapp_Template = "¡Hola! te escribimos para darte a conocer nuestros servicios de salud en el Centro de Salud Ancuya ESE. ¿Aceptas recibir esta información?:\n- Responde con el número *1* si quieres continuar\n- Responde con el número *2* si no quieres continuar"
+whatsapp_Template = "¡Hola! te escribimos para darte a conocer nuestros servicios de salud en la Red de Salud Ladera. ¿Aceptas recibir esta información?:\n- Responde con el número *1* si quieres continuar\n- Responde con el número *2* si no quieres continuar"
 # whatsapp_Template = "¡Atención mujer ancuyana! ¡Prioriza tu salud! Ven a realizarte la citología gratuita en el Centro de Salud Ancuya ESE. Horarios flexibles: Todas las tardes de lunes a viernes de 2-5 pm y los días Lunes, Miércoles, Viernes: 8-11 am. ¡Agenda tu cita previa ahora! Tel: 3174363751. ¡Cuidemos juntas nuestra salud!#CitologíaAncuyana"
 content_sid = 'HX1c9b04c28f67e2afbe2784b76caf6bcb'
 def crearTareaCampaña(campId,hora,minute,mId,tel=""):
@@ -153,7 +153,7 @@ def enviarWhatsapp(ID,mId):
     for u in usuariasCamp:
         res = resultadosxcampania(contacto_cc=u.contacto,campania_id=camp,medio_id=m,fecha=fechaActual)
         res.save()
-        clientWhatsapp.send_message(whatsapp_Template,"57"+u.contacto.celular,str(res.id), content_sid)
+        clientWhatsapp.send_message(whatsapp_Template,"57"+u.contacto.celular,str(res.id))
 
 
 
