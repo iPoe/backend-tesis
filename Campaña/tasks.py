629ecf01f7bbf0a641bf53250f0bc16422dac22a
Leo <leonardosawi12@gmail.com>
2024-04-17 13:17:13 -0500

    Change content sid for Week 6 25-29 camp

--- Campaña/tasks.py
+++ Campaña/tasks.py
@@ -19,7 +19,7 @@ clientSMS = SMS()
 clientVoice = VoiceCall()
 clientWhatsapp = WhatsApp()
 newWhatsappClient = WhatsApp()
-content_sid = 'HX5641590eeee71f8e3d5aef7e21032d55'
+content_sid = 'HX08603f7cc54170f0c1b55697e0b327d3'
 
 def crearTareaCampaña(campId,hora,minute,mId,tel=""):
     cam = Campania.objects.get(pk=campId)
